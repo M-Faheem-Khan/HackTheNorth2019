3e8d581c854442766a394130884b73fddb0e6960
Eric <eric11marcantonio@gmail.com>
2019-09-14 11:01:24 -0400

    cp

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
-
 import LoginScreen from "./Screens/loginScreen";
 import HomeScreen from "./Screens/HomeScreen";
+import FirstOpen from "./Screens/FirstOpen";
 
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 
-const AppNavigator = createStackNavigator({
-  Login: LoginScreen,
-  Home: HomeScreen
-});
+const AppNavigator = createStackNavigator(
+  {
+    //FirstOpen: FirstOpen,
+    Home: HomeScreen,
+    Login: LoginScreen
+  },
+  {
+    defaultNavigationOptions: {
+      headerTintColor: "#000",
+      headerStyle: {
+        backgroundColor: "#fff",
+        elevation: 0,
+        alignContent: "center",
+        display: "flex"
+      },
+      shadowColor: "transparent"
+    }
+  }
+);
 
 export default createAppContainer(AppNavigator);
